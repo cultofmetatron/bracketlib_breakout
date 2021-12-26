16830ffabc491ccdb8cd3cd0db40e22ddd57bcd9
cultofmetatron <cultofmetatron@aumlogic.com>
2021-12-26 17:42:12 -0500

    paddle moving along the screen

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,10 @@ miles
 
 */
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
+const SCREEN_WIDTH: isize = 80;
+const SCREEN_HEIGHT: isize = 50;
 const FRAME_DURATION: f32 = 75.0;
+const PADDLE_SIZE: isize = 5;
 
 enum GameMode {
     Playing,
@@ -45,6 +46,31 @@ impl Paddle {
                 x: 0,
                 y: 0,
             },
+        }
+    }
+    fn move_right(&mut self, x: isize) {
+        let new_position = self.x + x;
+
+        if new_position + PADDLE_SIZE < SCREEN_WIDTH {
+            self.x = new_position;
+        }
+    }
+    fn move_left(&mut self, x: isize) {
+        let new_position = self.x - x;
+
+        if new_position > 0 {
+            self.x = new_position;
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        for i in self.x..(self.x + PADDLE_SIZE) {
+            ctx.set(
+                i,
+                SCREEN_HEIGHT - 2,
+                BLACK,
+                WHITE,
+                to_cp437(' '),
+            );
         }
     }
 }
@@ -75,6 +101,9 @@ impl Ball {
             velocity: Velocity { x: 0, y: 0 },
         }
     }
+    fn set_velocity(&mut self, v: Velocity) {
+        self.velocity = v;
+    }
 }
 
 struct State {
@@ -95,8 +124,82 @@ impl State {
             score: 0,
         }
     }
+    fn restart(&mut self) {
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
+        self.score = 0;
+    }
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Game Over!");
+        ctx.print_centered(6, &format!("You earned {} points.", self.score));
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(10, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+
+        self.paddle.render(ctx);
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::Q => ctx.quitting = true,
+                VirtualKeyCode::Right => {
+                    //if self.paddle.x + PADDLE_SIZE < SCREEN_WIDTH {
+                    //    self.paddle = Paddle::new(self.paddle.x + 2);
+                    //}
+                    self.paddle.move_right(2);
+                }
+                VirtualKeyCode::Left => {
+                    //if self.paddle.x > 0 {
+                    //    self.paddle = Paddle::new(self.paddle.x - 2);
+                    //}
+                    self.paddle.move_left(2);
+                }
+                _ => {}
+            }
+        }
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Basic Breakout 2");
+        ctx.print_centered(8, "(p) play game");
+        ctx.print_centered(10, "(q) to quit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
 }
 
-fn main() {
-    println!("Hello, world!");
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx)
+        }
+    }
+}
+
+fn main() -> BError {
+    println!("Bootng Breakout");
+    let context = BTermBuilder::simple80x50()
+        .with_title("hello draggon")
+        .build()?;
+
+    main_loop(context, State::new())
 }
